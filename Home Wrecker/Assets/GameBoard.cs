2442204fdd19a0b06655c7df4b9974d9201091b3
Zachary Carreiro <43147444+Shadowlet@users.noreply.github.com>
2019-01-26 01:43:56 -0500

    Update GameBoard.cs

--- Home Wrecker/Assets/GameBoard.cs
+++ Home Wrecker/Assets/GameBoard.cs
@@ -9,15 +9,10 @@ public class GameBoard : MonoBehaviour
     public GameObject gameTiles;
     public Gametiles[,] tiles; //this is what holds the prefabs for the tiles
     public GameObject[] Tiles;
-    public GameObject[,] tileGrid;
-    public int[,] tileGridStatus;
-    private int rand_number;
 
     void Start()
     {
-        tiles          = new Gametiles[width,height];
-        tileGrid       = new GameObject[width, height];
-        tileGridStatus = new int[width, height];
+        tiles = new Gametiles[width,height];
         Board();
     }
 
@@ -33,6 +28,7 @@ public class GameBoard : MonoBehaviour
             for (int yy = 0; yy < height; yy++)
             {
                 Vector2 position = new Vector2(xx, yy);
+<<<<<<< HEAD
 
                 Map[xx] = Map[yy];
                 Instantiate(Tiles[Random.Range(0,7)], position, Quaternion.identity);
@@ -44,17 +40,11 @@ public class GameBoard : MonoBehaviour
                 Instantiate(Tiles[rand_number], position, Quaternion.identity);
                 tileGrid[xx, yy] = Tiles[rand_number];
                 tileGridStatus[xx, yy] = 0;
+=======
+                Instantiate(Tiles[Random.Range(0,7)], position, Quaternion.identity);
+>>>>>>> parent of 0ce953d... Update GameBoard.cs
             }
         }
-        for (int xx = 0; xx < width; xx++)
-        {
-            for (int yy = 0; yy < height; yy++)
-            {
-                Debug.Log("x:"+xx+ "y:"+yy+"-"+tileGrid[xx, yy]);
-                Debug.Log(tileGridStatus[xx, yy]);
-            }
-        }
-
     }
 
     private void Sweep()
