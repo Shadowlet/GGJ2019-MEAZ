93f302b0b8c5214ea0675c4e46a538f5eeda4e1b
Mattwalsh155 <41699810+Mattwalsh155@users.noreply.github.com>
2019-01-26 04:17:15 -0500

    instruction screen

---
+++ Home Wrecker/Assets/GameManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject menu;
+    public GameObject game;
+    public GameObject instructions;
+    public GameObject gameOver;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        menu.SetActive(true);
+        game.SetActive(false);
+        instructions.SetActive(false);
+        gameOver.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        menu.SetActive(false);
+        game.SetActive(true);
+        instructions.SetActive(false);
+        gameOver.SetActive(false);
+    }
+
+    public void Instructions()
+    {
+        menu.SetActive(false);
+        game.SetActive(false);
+        instructions.SetActive(true);
+        gameOver.SetActive(false);
+    }
+
+    public void GameOver()
+    {
+        gameOver.SetActive(true);
+    }
+}
