0ce953dd6535859ce8cbe51a5f2858b9921ccc88
ElsonHsieh <41646108+ElsonHsieh@users.noreply.github.com>
2019-01-26 01:34:58 -0500

    Update GameBoard.cs

--- Home Wrecker/Assets/GameBoard.cs
+++ Home Wrecker/Assets/GameBoard.cs
@@ -9,10 +9,15 @@ public class GameBoard : MonoBehaviour
     public GameObject gameTiles;
     public Gametiles[,] tiles; //this is what holds the prefabs for the tiles
     public GameObject[] Tiles;
+    public GameObject[,] tileGrid;
+    public int[,] tileGridStatus;
+    private int rand_number;
 
     void Start()
     {
-        tiles = new Gametiles[width,height];
+        tiles          = new Gametiles[width,height];
+        tileGrid       = new GameObject[width, height];
+        tileGridStatus = new int[width, height];
         Board();
     }
 
@@ -23,8 +28,20 @@ public class GameBoard : MonoBehaviour
             for (int yy = 0; yy < height; yy++)
             {
                 Vector2 position = new Vector2(xx, yy);
-                Instantiate(Tiles[Random.Range(0,7)], position, Quaternion.identity);
+                rand_number = Random.Range(0, 7);
+                Instantiate(Tiles[rand_number], position, Quaternion.identity);
+                tileGrid[xx, yy] = Tiles[rand_number];
+                tileGridStatus[xx, yy] = 0;
             }
         }
+        for (int xx = 0; xx < width; xx++)
+        {
+            for (int yy = 0; yy < height; yy++)
+            {
+                Debug.Log("x:"+xx+ "y:"+yy+"-"+tileGrid[xx, yy]);
+                Debug.Log(tileGridStatus[xx, yy]);
+            }
+        }
+
     }
 }
